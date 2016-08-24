ec115e6d01dbbaf924a6125d6770d436e09524ad
Peter Mitrano <mitranopeter@gmail.com>
2016-08-24 17:45:10 -0400

    added requests proper endpoints

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ zip:
 	mkdir -p build
 	rm -f ./build/skill.zip
 	cp -r ./lambda_function.py build
+	cp -r ./venv/lib/python2.7/site-packages/requests build
 	cd build &&	zip -q -r skill.zip *
 
 upload: zip
--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 
 LOGGER = 'my_desk'
 APP_ID = 'amzn1.ask.skill.999f5e91-7264-4660-b397-5efc340a51f9'
@@ -17,23 +18,36 @@ def handle_event(event, context):
             position = request['intent']['slots']['Position']['value']
             if position == 'sit':
                 logging.getLogger(LOGGER).warn('sit')
+                command_endpoint = 'position/sit'
             elif position == 'stand':
                 logging.getLogger(LOGGER).warn('stand')
+                command_endpoint = 'position/stand'
         elif intent == 'GoToHeightIntent':
             height = request['intent']['slots']['Height']['value']
             logging.getLogger(LOGGER).warn(height)
+            command_endpoint = 'height/%i' % height
         elif intent == 'GoUpIntent':
             logging.getLogger(LOGGER).warn('up')
+            command_endpoint = 'up'
         elif intent == 'GoDownIntent':
             logging.getLogger(LOGGER).warn('down')
+            command_endpoint = 'down'
         elif intent == 'AMAZON.StopIntent':
             logging.getLogger(LOGGER).warn('stop')
+            command_endpoint = 'stop'
 
-    response = {
-        "version": 1.0,
-        "response": {
+    if requests.get('http://66.189.43.74:3776/' + command_endpoint).ok:
+        response = { "version": 1.0, "response": { } }
+    else:
+        response = {
+            "version": 1.0,
+            "response": {
+                "outputSpeech": {
+                    "type": "SSML",
+                    "ssml": "I'm sorry dave, I'm afraid I can't do that."
+                }
+            }
         }
-    }
 
     logging.getLogger(LOGGER).warn(response)
     return response
