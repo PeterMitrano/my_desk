5f2509016dbe770e6fd4d812e27ce2b50345e23e
Peter Mitrano <mitranopeter@gmail.com>
2016-08-24 03:05:32 -0400

    added basic logic

--- lambda_function.py
+++ lambda_function.py
@@ -9,13 +9,29 @@ def handle_event(event, context):
     if event['session']['application']['applicationId'] != APP_ID:
         raise RuntimeError('Wrong applicationId')
 
+    request = event['request']
+    if 'intent' in request:
+        intent = request['intent']['name']
+
+        if intent == 'GoToPositionIntent':
+            position = request['intent']['slots']['Position']['value']
+            if position == 'sit':
+                logging.getLogger(LOGGER).warn('sit')
+            elif position == 'stand':
+                logging.getLogger(LOGGER).warn('stand')
+        elif intent == 'GoToHeightIntent':
+            height = request['intent']['slots']['Height']['value']
+            logging.getLogger(LOGGER).warn(height)
+        elif intent == 'GoUpIntent':
+            logging.getLogger(LOGGER).warn('up')
+        elif intent == 'GoDownIntent':
+            logging.getLogger(LOGGER).warn('down')
+        elif intent == 'AMAZON.StopIntent':
+            logging.getLogger(LOGGER).warn('stop')
+
     response = {
         "version": 1.0,
         "response": {
-            "outputSpeech": {
-                "type": "SSML",
-                "ssml": "hello world"
-            }
         }
     }
 
